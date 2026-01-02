651108f6581ee1bf86aec50807ce29b22f7e425f
solutionneurs228 <solutionneurs228@gmail.com>
2026-01-02 17:04:33 +0000

    galerie accueil

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 function name(params) {
-    
+
 }
 
 const fn = (p1) => {
-    
+
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -417,18 +417,27 @@
 
                 <!-- Vidéo -->
                 <div class="gallery-item video">
-                    <video muted preload="metadata">
-                        <source src="../videos/moba.mp4" type="video/mp4">
+                    <video controls preload="metadata">
+                        <source src="{{ asset('videos/moba.mp4') }}" type="video/mp4">
                     </video>
+                    <span class="play-icon">▶</span>
+                </div>
+                <!-- Vidéo -->
+                <div class="gallery-item video">
+                    <iframe width="100%" height="100%" src="https://www.youtube.com/embed/hRNwlx2lE9k"
+                        title="S228/la prestation de la chorale Salut des âmes des AD tchannadè lors du mariage de yovo &amp; Immaculée"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                     <span class="play-icon">▶</span>
                 </div>
 
-                <div class="gallery-item video">
-                    <video muted preload="metadata">
+                {{-- <div class="gallery-item video">
+                    <video preload="metadata">
                         <source src="../images/shadai.mp4" type="video/mp4">
                     </video>
                     <span class="play-icon">▶</span>
-                </div>
+                </div> --}}
             </div>
 
             <div class="gallery-action">
