5cc242d8777b5471fb7017314d7462a55c06ad66
solutionneurs228 <solutionneurs228@gmail.com>
2026-01-21 22:47:55 +0000

    ras

--- app/Http/Controllers/DevisController.php
+++ app/Http/Controllers/DevisController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\models\Devis;
+use App\Models\Devis;
 use Illuminate\Support\Facades\Route;
 use App\Mail\DevisMail;
 use Illuminate\Support\Facades\Mail;
